387c0ecc4fb64d0e930bf346effb63d651b01d07
Alexander199824 <mismaleje@miumg.edu.gt>
2024-11-01 00:55:42 -0600

    cambioruter

--- app/routers/clientRoutes.js
+++ app/routers/clientRoutes.js
@@ -11,8 +11,8 @@ const {
 } = require('../controllers/client.controller');
 const authMiddleware = require('../middlewares/authMiddleware'); // Importa authMiddleware
 
-// Crear un nuevo cliente con soporte de imagen
-router.post('/', authMiddleware, upload.single('image'), createClient);
+// Crear un nuevo cliente con soporte de imagen, sin requerir autenticación
+router.post('/', upload.single('image'), createClient);
 
 // Obtener todos los clientes
 router.get('/', authMiddleware, getAllClients);
