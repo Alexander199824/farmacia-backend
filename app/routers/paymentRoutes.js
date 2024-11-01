fd009d5611482ceced11aef63001d8534e58c76a
Alexander199824 <mismaleje@miumg.edu.gt>
2024-11-01 09:01:28 -0600

    front pagos 2

--- app/routers/paymentRoutes.js
+++ app/routers/paymentRoutes.js
@@ -2,9 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const paymentController = require('../controllers/payment.controller');
-const authMiddleware = require('../middlewares/authMiddleware'); // Importa el middleware de autenticación
 
-// Ruta para crear PaymentIntent (protegida con authMiddleware)
-router.post('/create-payment-intent', authMiddleware, paymentController.createPaymentIntent);
+// Ruta para crear PaymentIntent sin autenticación
+router.post('/create-payment-intent', paymentController.createPaymentIntent);
 
 module.exports = router;
